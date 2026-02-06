f2775cf8f7f4422de7836806d30e1297e597f666
danushvdarshan <danushvdarshan0000@gmail.com>
2026-02-07 01:41:55 +0530

    Add files via upload

--- backend/calendar_builder.py
+++ backend/calendar_builder.py
@@ -1,40 +1,37 @@
-# calendar_builder.py
-
-DAYS = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
-
 def build_events(schedule):
+    DAYS = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
     events = []
 
     for day_idx, day_slots in enumerate(schedule):
-        current_activity = None
+        current_cell = None
         start_hour = None
 
         for hour in range(24):
-            slot = day_slots[hour]
+            cell = day_slots[hour]
 
-            if slot != current_activity:
-                if current_activity is not None:
+            if cell != current_cell:
+                if current_cell is not None:
                     events.append({
                         "day": DAYS[day_idx],
                         "start_hour": start_hour,
                         "end_hour": hour,
-                        "activity": current_activity
+                        "activity": current_cell
                     })
 
-                if slot is not None:
-                    current_activity = slot
+                if cell is not None:
+                    current_cell = cell
                     start_hour = hour
                 else:
-                    current_activity = None
+                    current_cell = None
                     start_hour = None
 
-        if current_activity is not None:
+        # end of day
+        if current_cell is not None:
             events.append({
                 "day": DAYS[day_idx],
                 "start_hour": start_hour,
                 "end_hour": 24,
-                "activity": current_activity
+                "activity": current_cell
             })
 
     return events
-
